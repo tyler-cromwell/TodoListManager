87f4d2e483be41f5d76ec96654c96f39dbefbb86
Souley Bane <souleyman.bane@gmail.com>
2016-05-09 12:56:26 -0400

    Task Object

--- src/todolistmanager/Task.java
+++ src/todolistmanager/Task.java
@@ -4,5 +4,13 @@ package todolistmanager;
  * @author Souley Bane
  */
 public class Task {
+    private String taskDetails;
     
+    public Task(String task){
+        taskDetails = task;
+    } 
+    
+    public String getText(){
+        return taskDetails;
+    }
 }
